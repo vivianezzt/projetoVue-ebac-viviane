f593ca416e8c54f3aedaa0bb49379a642c4bb244
Viviane <vivianeaguiarc@outlook.com>
2024-05-30 11:19:29 -0300

    Atualização

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -4,16 +4,14 @@
 
 
 <template>
-    <div>
-    <ul class="list-group mt-4" v-if="props.tarefas.length > 0">
-        <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
-            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
-        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
-        </li>
+    <ul class="list-group mt-4">
+      <li class="list-group-item mb-1" v-for="tarefa in props.tarefas">
+        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+        <label :class="{ done: tarefa.finalizada }" class="ms-3 item" :for="tarefa.titulo">
+          {{ tarefa.titulo }}
+        </label>
+      </li>
     </ul>
-  <p v-else>Não existe tarefas pendentes</p>
-</div>
-
 </template>
 
 <style scoped>
